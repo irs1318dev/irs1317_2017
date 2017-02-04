122458122d815e82f70317fb3b78f8acf6952a4e
Driver Station 2 <Driver Station 2@10.100.27.40>
2017-02-04 15:07:38 -0800

    Added examples of assert testing in test class

--- irs1318_2017/test/org/usfirst/frc/team1318/robot/intake/IntakeComponentTest.java
+++ irs1318_2017/test/org/usfirst/frc/team1318/robot/intake/IntakeComponentTest.java
@@ -1,9 +1,11 @@
 package org.usfirst.frc.team1318.robot.intake;
 
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.doReturn;
 
 import org.junit.Test;
 import org.usfirst.frc.team1318.robot.common.wpilibmocks.DoubleSolenoidValue;
@@ -37,10 +39,19 @@ public class IntakeComponentTest
         IDoubleSolenoid intakeSolenoid = mock(IDoubleSolenoid.class);
         ISolenoid intakeLight = mock(ISolenoid.class);
         IAnalogInput throughBeamSensor = mock(IAnalogInput.class);
+        
+        // Mock/fake a return value when get method is called
+        doReturn(1.0).when(throughBeamSensor.getVoltage());
 
         IntakeComponent intakeComponent = new IntakeComponent(motor, intakeSolenoid, intakeLight, throughBeamSensor);
 
         intakeComponent.extendOrRetract(false);
+        
+        // Assert true/false when method that returns true/false is called
+        assertTrue(intakeComponent.getThroughBeamBroken());
+        
+        // Assert double value is equal when get method is called, within acceptable range
+        assertEquals(3.0/*expected*/, 3.001/*actual, the mocked return value from a get method*/, 0.001 /*allowed error*/);
 
         verify(intakeSolenoid).set(eq(DoubleSolenoidValue.kReverse));
         verifyNoMoreInteractions(motor, intakeSolenoid, intakeLight, throughBeamSensor);
