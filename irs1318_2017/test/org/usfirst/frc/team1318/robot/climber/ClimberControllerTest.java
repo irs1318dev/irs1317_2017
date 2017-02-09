e038b859f512963752f2eaa0947efec10924f7de
Driver Station 2 <Driver Station 2@WINDOWS-SI3N1V9>
2017-02-08 20:17:28 -0800

    Fixed errors in ClimberControllerTest and tested it

--- irs1318_2017/test/org/usfirst/frc/team1318/robot/climber/ClimberControllerTest.java
+++ irs1318_2017/test/org/usfirst/frc/team1318/robot/climber/ClimberControllerTest.java
@@ -1,68 +1,79 @@
 package org.usfirst.frc.team1318.robot.climber;
 
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import org.junit.Test;
 import org.usfirst.frc.team1318.robot.TuningConstants;
+import org.usfirst.frc.team1318.robot.driver.Driver;
 import org.usfirst.frc.team1318.robot.driver.Operation;
 
 public class ClimberControllerTest
 {
     @Test
-    public void testUpdate_SetClimberSpeed_0() {
+    public void testUpdate_SetClimberSpeed_0()
+    {
         ClimberComponent climber = mock(ClimberComponent.class);
         Driver driver = mock(Driver.class);
-        
+
         doReturn(0.0).when(driver).getAnalog(Operation.ClimberSpeed);
-        
-        ClimberController controller= new ClimberController(climber);
+
+        ClimberController controller = new ClimberController(climber);
         controller.setDriver(driver);
-        
+
         controller.update();
-        
+
         verify(climber).setMotorSpeed(0.0);
         verifyNoMoreInteractions(climber);
     }
-    
+
     @Test
-    public void testUpdate_SetClimberSpeed_MAX_CLIMBER_SPEED() {
+    public void testUpdate_SetClimberSpeed_MAX_CLIMBER_SPEED()
+    {
         ClimberComponent climber = mock(ClimberComponent.class);
         Driver driver = mock(Driver.class);
-        
+
         doReturn(1.0).when(driver).getAnalog(Operation.ClimberSpeed);
-        
-        ClimberController controller= new ClimberController(climber);
+
+        ClimberController controller = new ClimberController(climber);
         controller.setDriver(driver);
-        
+
         controller.update();
-        
+
         verify(climber).setMotorSpeed(TuningConstants.CLIMBER_MAX_MOTOR_POWER);
         verifyNoMoreInteractions(climber);
     }
-    
+
     @Test
-    public void testUpdate_SetClimberSpeed_MIN_CLIMBER_SPEED() {
+    public void testUpdate_SetClimberSpeed_MIN_CLIMBER_SPEED()
+    {
         ClimberComponent climber = mock(ClimberComponent.class);
         Driver driver = mock(Driver.class);
-        
+
         doReturn(-1.0).when(driver).getAnalog(Operation.ClimberSpeed);
-        
-        ClimberController controller= new ClimberController(climber);
+
+        ClimberController controller = new ClimberController(climber);
         controller.setDriver(driver);
-        
+
         controller.update();
-        
+
         verify(climber).setMotorSpeed(-TuningConstants.CLIMBER_MAX_MOTOR_POWER);
         verifyNoMoreInteractions(climber);
     }
-    
+
     @Test
-    public void testStop() {
+    public void testStop()
+    {
         ClimberComponent climber = mock(ClimberComponent.class);
         Driver driver = mock(Driver.class);
-        
-        ClimberController controller= new ClimberController(climber);
+
+        ClimberController controller = new ClimberController(climber);
         controller.setDriver(driver);
-        
+
         controller.stop();
-        
+
         verify(climber).stop();
         verifyNoMoreInteractions(climber);
     }
