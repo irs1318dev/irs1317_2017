e15745ab4802f08bee37f83cf987c7425bbf9a13
Driver Station 2 <Driver Station 2@WINDOWS-SI3N1V9>
2017-02-08 20:02:44 -0800

    Fixed error in IntakeControllerTest and tested it

--- irs1318_2017/test/org/usfirst/frc/team1318/robot/intake/IntakeControllerTest.java
+++ irs1318_2017/test/org/usfirst/frc/team1318/robot/intake/IntakeControllerTest.java
@@ -1,5 +1,12 @@
 package org.usfirst.frc.team1318.robot.intake;
 
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import org.junit.Test;
 import org.usfirst.frc.team1318.robot.TuningConstants;
 import org.usfirst.frc.team1318.robot.driver.Driver;
 import org.usfirst.frc.team1318.robot.driver.Operation;
@@ -7,166 +14,174 @@ import org.usfirst.frc.team1318.robot.driver.Operation;
 public class IntakeControllerTest
 {
     @Test
-    public void testUpdate_doNothing() {
+    public void testUpdate_doNothing()
+    {
         IntakeComponent intake = mock(IntakeComponent.class);
         Driver driver = mock(Driver.class);
-        
+
         doReturn(false).when(driver).getDigital(Operation.IntakeArmExtend);
         doReturn(false).when(driver).getDigital(Operation.IntakeArmRetract);
         doReturn(false).when(driver).getDigital(Operation.IntakeGearHolderExtend);
         doReturn(false).when(driver).getDigital(Operation.IntakeGearHolderRetract);
         doReturn(false).when(driver).getDigital(Operation.IntakeIn);
         doReturn(false).when(driver).getDigital(Operation.IntakeOut);
-        
+
         IntakeController controller = new IntakeController(intake);
         controller.setDriver(driver);
-        
+
         controller.update();
-        
-        verify(intake).setIntakePower(0.0);
+
+        verify(intake).setMotorSpeed(0.0);
         verifyNoMoreInteractions(intake);
     }
-    
+
     @Test
-    public void testUpdate_IntakeArmExtend() {
+    public void testUpdate_IntakeArmExtend()
+    {
         IntakeComponent intake = mock(IntakeComponent.class);
         Driver driver = mock(Driver.class);
-        
+
         doReturn(true).when(driver).getDigital(Operation.IntakeArmExtend);
         doReturn(false).when(driver).getDigital(Operation.IntakeArmRetract);
         doReturn(false).when(driver).getDigital(Operation.IntakeGearHolderExtend);
         doReturn(false).when(driver).getDigital(Operation.IntakeGearHolderRetract);
         doReturn(false).when(driver).getDigital(Operation.IntakeIn);
         doReturn(false).when(driver).getDigital(Operation.IntakeOut);
-        
+
         IntakeController controller = new IntakeController(intake);
         controller.setDriver(driver);
-        
+
         controller.update();
-        
-        verify(intake).setIntakePower(0.0);
+
+        verify(intake).setMotorSpeed(0.0);
         verify(intake).extendIntake(eq(true));
         verifyNoMoreInteractions(intake);
     }
-    
+
     @Test
-    public void testUpdate_IntakeArmRetract() {
+    public void testUpdate_IntakeArmRetract()
+    {
         IntakeComponent intake = mock(IntakeComponent.class);
         Driver driver = mock(Driver.class);
-        
+
         doReturn(false).when(driver).getDigital(Operation.IntakeArmExtend);
         doReturn(true).when(driver).getDigital(Operation.IntakeArmRetract);
         doReturn(false).when(driver).getDigital(Operation.IntakeGearHolderExtend);
         doReturn(false).when(driver).getDigital(Operation.IntakeGearHolderRetract);
         doReturn(false).when(driver).getDigital(Operation.IntakeIn);
         doReturn(false).when(driver).getDigital(Operation.IntakeOut);
-        
+
         IntakeController controller = new IntakeController(intake);
         controller.setDriver(driver);
-        
+
         controller.update();
-        
-        verify(intake).setIntakePower(0.0);
-        verify(intake).retractIntake(eq(true));
+
+        verify(intake).setMotorSpeed(0.0);
+        verify(intake).extendIntake(eq(false));
         verifyNoMoreInteractions(intake);
     }
-    
+
     @Test
-    public void testUpdate_IntakeGearHolderExtend() {
+    public void testUpdate_IntakeGearHolderExtend()
+    {
         IntakeComponent intake = mock(IntakeComponent.class);
         Driver driver = mock(Driver.class);
-        
+
         doReturn(false).when(driver).getDigital(Operation.IntakeArmExtend);
         doReturn(false).when(driver).getDigital(Operation.IntakeArmRetract);
         doReturn(true).when(driver).getDigital(Operation.IntakeGearHolderExtend);
         doReturn(false).when(driver).getDigital(Operation.IntakeGearHolderRetract);
         doReturn(false).when(driver).getDigital(Operation.IntakeIn);
         doReturn(false).when(driver).getDigital(Operation.IntakeOut);
-        
+
         IntakeController controller = new IntakeController(intake);
         controller.setDriver(driver);
-        
+
         controller.update();
-        
-        verify(intake).setIntakePower(0.0);
+
+        verify(intake).setMotorSpeed(0.0);
         verify(intake).extendGearHolder(eq(true));
         verifyNoMoreInteractions(intake);
     }
-    
+
     @Test
-    public void testUpdate_IntakeGearHolderRetract() {
+    public void testUpdate_IntakeGearHolderRetract()
+    {
         IntakeComponent intake = mock(IntakeComponent.class);
         Driver driver = mock(Driver.class);
-        
+
         doReturn(false).when(driver).getDigital(Operation.IntakeArmExtend);
         doReturn(false).when(driver).getDigital(Operation.IntakeArmRetract);
         doReturn(false).when(driver).getDigital(Operation.IntakeGearHolderExtend);
         doReturn(true).when(driver).getDigital(Operation.IntakeGearHolderRetract);
         doReturn(false).when(driver).getDigital(Operation.IntakeIn);
         doReturn(false).when(driver).getDigital(Operation.IntakeOut);
-        
+
         IntakeController controller = new IntakeController(intake);
         controller.setDriver(driver);
-        
+
         controller.update();
-        
-        verify(intake).setIntakePower(0.0);
-        verify(intake).retractGearHolder(eq(true));
+
+        verify(intake).setMotorSpeed(0.0);
+        verify(intake).extendGearHolder(eq(false));
         verifyNoMoreInteractions(intake);
     }
-    
+
     @Test
-    public void testUpdate_IntakeIn() {
+    public void testUpdate_IntakeIn()
+    {
         IntakeComponent intake = mock(IntakeComponent.class);
         Driver driver = mock(Driver.class);
-        
+
         doReturn(false).when(driver).getDigital(Operation.IntakeArmExtend);
         doReturn(false).when(driver).getDigital(Operation.IntakeArmRetract);
         doReturn(false).when(driver).getDigital(Operation.IntakeGearHolderExtend);
         doReturn(false).when(driver).getDigital(Operation.IntakeGearHolderRetract);
         doReturn(true).when(driver).getDigital(Operation.IntakeIn);
         doReturn(false).when(driver).getDigital(Operation.IntakeOut);
-        
+
         IntakeController controller = new IntakeController(intake);
         controller.setDriver(driver);
-        
+
         controller.update();
-        
-        verify(intake).setIntakePower(TuningConstants.INTAKE_MAX_MOTOR_SPEED);
+
+        verify(intake).setMotorSpeed(TuningConstants.INTAKE_MAX_MOTOR_SPEED);
         verifyNoMoreInteractions(intake);
     }
-    
+
     @Test
-    public void testUpdate_IntakeOut() {
+    public void testUpdate_IntakeOut()
+    {
         IntakeComponent intake = mock(IntakeComponent.class);
         Driver driver = mock(Driver.class);
-        
+
         doReturn(false).when(driver).getDigital(Operation.IntakeArmExtend);
         doReturn(false).when(driver).getDigital(Operation.IntakeArmRetract);
         doReturn(false).when(driver).getDigital(Operation.IntakeGearHolderExtend);
         doReturn(false).when(driver).getDigital(Operation.IntakeGearHolderRetract);
         doReturn(false).when(driver).getDigital(Operation.IntakeIn);
         doReturn(true).when(driver).getDigital(Operation.IntakeOut);
-        
+
         IntakeController controller = new IntakeController(intake);
         controller.setDriver(driver);
-        
+
         controller.update();
-        
-        verify(intake).setIntakePower(-TuningConstants.INTAKE_MAX_MOTOR_SPEED);
+
+        verify(intake).setMotorSpeed(-TuningConstants.INTAKE_MAX_MOTOR_SPEED);
         verifyNoMoreInteractions(intake);
     }
-    
-    @test
-    public void testStop() {
+
+    @Test
+    public void testStop()
+    {
         IntakeComponent intake = mock(IntakeComponent.class);
         Driver driver = mock(Driver.class);
-        
+
         IntakeController controller = new IntakeController(intake);
         controller.setDriver(driver);
-        
+
         controller.stop();
-        
+
         verify(intake).stop();
         verifyNoMoreInteractions(intake);
     }
