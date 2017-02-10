c88e8bbe928777df3c3ec7dde098d80005962219
Driver Station 2 <Driver Station 2@WINDOWS-SI3N1V9>
2017-02-09 19:20:34 -0800

    Added log for climber current draw

--- irs1318_2017/src/org/usfirst/frc/team1318/robot/climber/ClimberController.java
+++ irs1318_2017/src/org/usfirst/frc/team1318/robot/climber/ClimberController.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team1318.robot.climber;
 import org.usfirst.frc.team1318.robot.ElectronicsConstants;
 import org.usfirst.frc.team1318.robot.TuningConstants;
 import org.usfirst.frc.team1318.robot.common.IController;
+import org.usfirst.frc.team1318.robot.common.IDashboardLogger;
 import org.usfirst.frc.team1318.robot.driver.Driver;
 import org.usfirst.frc.team1318.robot.driver.Operation;
 import org.usfirst.frc.team1318.robot.general.PowerManager;
@@ -11,13 +12,16 @@ import com.google.inject.Inject;
 
 public class ClimberController implements IController
 {
+    private final String LogName = "climber";
+    private final IDashboardLogger logger;
     private final ClimberComponent climber;
     private final PowerManager powerManager;
     private Driver driver;
 
     @Inject
-    public ClimberController(ClimberComponent climber, PowerManager powerManager)
+    public ClimberController(IDashboardLogger logger, ClimberComponent climber, PowerManager powerManager)
     {
+        this.logger = logger;
         this.climber = climber;
         this.powerManager = powerManager;
     }
@@ -28,6 +32,7 @@ public class ClimberController implements IController
         double climberSpeed = this.driver.getAnalog(Operation.ClimberSpeed);
         double currentDraw = this.powerManager.getCurrent(ElectronicsConstants.CLIMBER_PDP_CHANNEL_A) +
             this.powerManager.getCurrent(ElectronicsConstants.CLIMBER_PDP_CHANNEL_B);
+        this.logger.logNumber(LogName, "current", currentDraw);
         if (currentDraw > TuningConstants.CLIMBER_MAX_CURRENT_DRAW)
         {
             climberSpeed = 0.0;
