8c95e02959065eeeb24f78e0d6452efdc45d8e0b
Driver Station 2 <Driver Station 2@WINDOWS-SI3N1V9>
2017-02-09 17:40:07 -0800

    Added dashboardlogger for climber

--- irs1318_2017/src/org/usfirst/frc/team1318/robot/climber/ClimberComponent.java
+++ irs1318_2017/src/org/usfirst/frc/team1318/robot/climber/ClimberComponent.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team1318.robot.climber;
 
+import org.usfirst.frc.team1318.robot.common.IDashboardLogger;
 import org.usfirst.frc.team1318.robot.common.wpilibmocks.IMotor;
 
 import com.google.inject.Inject;
@@ -10,16 +11,20 @@ import com.google.inject.name.Named;
 public class ClimberComponent
 {
     private IMotor motor;
+    private final IDashboardLogger logger;
+    private final String key = "climber";
 
     @Inject
-    public ClimberComponent(@Named("CLIMBER_MOTOR") IMotor motor)
+    public ClimberComponent(IDashboardLogger logger, @Named("CLIMBER_MOTOR") IMotor motor)
     {
+        this.logger = logger;
         this.motor = motor;
     }
 
     public void setMotorSpeed(double speed)
     {
         this.motor.set(speed);
+        this.logger.logNumber(key, "climberSpeed", speed);
     }
 
     public void stop()
