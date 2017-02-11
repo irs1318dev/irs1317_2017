af1e0c862c9d3dfa8854b44c843e3f9cf85ef817
WRall <wm_rall@hotmail.com>
2017-02-10 19:07:47 -0800

    Update Climber controller

--- irs1318_2017/test/org/usfirst/frc/team1318/robot/climber/ClimberControllerTest.java
+++ irs1318_2017/test/org/usfirst/frc/team1318/robot/climber/ClimberControllerTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import org.junit.Test;
 import org.usfirst.frc.team1318.robot.ElectronicsConstants;
 import org.usfirst.frc.team1318.robot.TuningConstants;
+import org.usfirst.frc.team1318.robot.common.IDashboardLogger;
 import org.usfirst.frc.team1318.robot.driver.Driver;
 import org.usfirst.frc.team1318.robot.driver.Operation;
 import org.usfirst.frc.team1318.robot.general.PowerManager;
@@ -17,15 +18,16 @@ public class ClimberControllerTest
     @Test
     public void testUpdate_SetClimberSpeed_0()
     {
+        IDashboardLogger dashboardLogger = mock(IDashboardLogger.class);
         ClimberComponent climber = mock(ClimberComponent.class);
-        Driver driver = mock(Driver.class);
         PowerManager powerManager = mock(PowerManager.class);
+        Driver driver = mock(Driver.class);
 
         doReturn(0.0).when(driver).getAnalog(Operation.ClimberSpeed);
         doReturn(0.0).when(powerManager).getCurrent(ElectronicsConstants.CLIMBER_PDP_CHANNEL_A);
         doReturn(0.0).when(powerManager).getCurrent(ElectronicsConstants.CLIMBER_PDP_CHANNEL_B);
 
-        ClimberController controller = new ClimberController(climber, powerManager);
+        ClimberController controller = new ClimberController(dashboardLogger, climber, powerManager);
         controller.setDriver(driver);
 
         controller.update();
@@ -37,15 +39,16 @@ public class ClimberControllerTest
     @Test
     public void testUpdate_SetClimberSpeed_MAX_CLIMBER_SPEED()
     {
+        IDashboardLogger dashboardLogger = mock(IDashboardLogger.class);
         ClimberComponent climber = mock(ClimberComponent.class);
-        Driver driver = mock(Driver.class);
         PowerManager powerManager = mock(PowerManager.class);
+        Driver driver = mock(Driver.class);
 
         doReturn(1.0).when(driver).getAnalog(Operation.ClimberSpeed);
         doReturn(0.0).when(powerManager).getCurrent(ElectronicsConstants.CLIMBER_PDP_CHANNEL_A);
         doReturn(0.0).when(powerManager).getCurrent(ElectronicsConstants.CLIMBER_PDP_CHANNEL_B);
 
-        ClimberController controller = new ClimberController(climber, powerManager);
+        ClimberController controller = new ClimberController(dashboardLogger, climber, powerManager);
         controller.setDriver(driver);
 
         controller.update();
@@ -57,15 +60,16 @@ public class ClimberControllerTest
     @Test
     public void testUpdate_SetClimberSpeed_MIN_CLIMBER_SPEED()
     {
+        IDashboardLogger dashboardLogger = mock(IDashboardLogger.class);
         ClimberComponent climber = mock(ClimberComponent.class);
-        Driver driver = mock(Driver.class);
         PowerManager powerManager = mock(PowerManager.class);
+        Driver driver = mock(Driver.class);
 
         doReturn(-1.0).when(driver).getAnalog(Operation.ClimberSpeed);
         doReturn(0.0).when(powerManager).getCurrent(ElectronicsConstants.CLIMBER_PDP_CHANNEL_A);
         doReturn(0.0).when(powerManager).getCurrent(ElectronicsConstants.CLIMBER_PDP_CHANNEL_B);
 
-        ClimberController controller = new ClimberController(climber, powerManager);
+        ClimberController controller = new ClimberController(dashboardLogger, climber, powerManager);
         controller.setDriver(driver);
 
         controller.update();
@@ -77,15 +81,16 @@ public class ClimberControllerTest
     @Test
     public void testUpdate_OverDrawCurrent()
     {
+        IDashboardLogger dashboardLogger = mock(IDashboardLogger.class);
         ClimberComponent climber = mock(ClimberComponent.class);
-        Driver driver = mock(Driver.class);
         PowerManager powerManager = mock(PowerManager.class);
+        Driver driver = mock(Driver.class);
 
         doReturn(-1.0).when(driver).getAnalog(Operation.ClimberSpeed);
         doReturn(1.0).when(powerManager).getCurrent(ElectronicsConstants.CLIMBER_PDP_CHANNEL_A);
         doReturn(1.0).when(powerManager).getCurrent(ElectronicsConstants.CLIMBER_PDP_CHANNEL_B);
 
-        ClimberController controller = new ClimberController(climber, powerManager);
+        ClimberController controller = new ClimberController(dashboardLogger, climber, powerManager);
         controller.setDriver(driver);
 
         controller.update();
@@ -97,11 +102,12 @@ public class ClimberControllerTest
     @Test
     public void testStop()
     {
+        IDashboardLogger dashboardLogger = mock(IDashboardLogger.class);
         ClimberComponent climber = mock(ClimberComponent.class);
-        Driver driver = mock(Driver.class);
         PowerManager powerManager = mock(PowerManager.class);
+        Driver driver = mock(Driver.class);
 
-        ClimberController controller = new ClimberController(climber, powerManager);
+        ClimberController controller = new ClimberController(dashboardLogger, climber, powerManager);
         controller.setDriver(driver);
 
         controller.stop();
