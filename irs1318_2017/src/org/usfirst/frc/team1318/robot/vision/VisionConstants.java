1e2f7268616953bc4de969a9c25fc8a4dc59fed6
WRall <wm_rall@hotmail.com>
2017-02-13 19:42:36 -0800

    Making vision changes based off of experimentation

--- irs1318_2017/src/org/usfirst/frc/team1318/robot/vision/VisionConstants.java
+++ irs1318_2017/src/org/usfirst/frc/team1318/robot/vision/VisionConstants.java
@@ -5,13 +5,14 @@ import org.opencv.core.Scalar;
 public class VisionConstants
 {
     // Debug output settings:
-    public static final boolean DEBUG = true;
-    public static final boolean DEBUG_PRINT_OUTPUT = true;
+    public static final boolean DEBUG = false;
+    public static final boolean DEBUG_PRINT_OUTPUT = false;
     public static final boolean DEBUG_PRINT_ANALYZER_DATA = false;
     public static final int DEBUG_FPS_AVERAGING_INTERVAL = 25;
-    public static final boolean DEBUG_FRAME_OUTPUT = false;
+    public static final boolean DEBUG_SAVE_FRAMES = false;
+    public static final boolean DEBUG_OUTPUT_FRAMES = false;
     public static final int DEBUG_FRAME_OUTPUT_GAP = 25; // the number of frames to wait between saving debug image output
-    public static final String DEBUG_OUTPUT_FOLDER = "/home/lvuser/vision/";
+    public static final String DEBUG_OUTPUT_FOLDER = "/C/vision/";
 
     // Conversion constants...
     public static final double ANGLE_TO_RADIANS = (Math.PI / 180.0f);
@@ -34,8 +35,10 @@ public class VisionConstants
     public static final int LIFECAM_CAMERA_CENTER_HEIGHT = VisionConstants.LIFECAM_CAMERA_RESOLUTION_Y / 2; // distance from center to top/bottom in pixels
     public static final double LIFECAM_CAMERA_FIELD_OF_VIEW_X = 48.4; // 4:3 field of view along x axis. note that documentation says 68.5 degrees diagonal (at 16:9), so this is an estimate.
     public static final double LIFECAM_CAMERA_FIELD_OF_VIEW_Y = 36.3; // 4:3 field of view along y axis
-    public static final double LIFECAM_CAMERA_FIELD_OF_VIEW_X_RADIANS = VisionConstants.LIFECAM_CAMERA_FIELD_OF_VIEW_X * VisionConstants.ANGLE_TO_RADIANS;
-    public static final double LIFECAM_CAMERA_FIELD_OF_VIEW_Y_RADIANS = VisionConstants.LIFECAM_CAMERA_FIELD_OF_VIEW_Y * VisionConstants.ANGLE_TO_RADIANS;
+    public static final double LIFECAM_CAMERA_FIELD_OF_VIEW_X_RADIANS = VisionConstants.LIFECAM_CAMERA_FIELD_OF_VIEW_X
+        * VisionConstants.ANGLE_TO_RADIANS;
+    public static final double LIFECAM_CAMERA_FIELD_OF_VIEW_Y_RADIANS = VisionConstants.LIFECAM_CAMERA_FIELD_OF_VIEW_Y
+        * VisionConstants.ANGLE_TO_RADIANS;
     public static final double LIFECAM_CAMERA_CENTER_VIEW_ANGLE = VisionConstants.LIFECAM_CAMERA_FIELD_OF_VIEW_X / 2.0;
     public static final int LIFECAM_CAMERA_EXPOSURE = 30;
     public static final int LIFECAM_CAMERA_BRIGHTNESS = 30;
@@ -47,11 +50,11 @@ public class VisionConstants
     // HSV Filtering constants
     public static final Scalar AXIS_HSV_FILTER_LOW = new Scalar(85, 65, 65);
     public static final Scalar AXIS_HSV_FILTER_HIGH = new Scalar(90, 255, 255);
-    public static final Scalar LIFECAM_HSV_FILTER_LOW = new Scalar(75, 100, 100);
+    public static final Scalar LIFECAM_HSV_FILTER_LOW = new Scalar(60, 100, 100);
     public static final Scalar LIFECAM_HSV_FILTER_HIGH = new Scalar(90, 255, 255);
 
     // Contour filtering constants
-    public static final double CONTOUR_MIN_AREA = 125.0;
+    public static final double CONTOUR_MIN_AREA = 25.0;
 
     // Real Measurements
     public static final double REAL_GEAR_RETROREFLECTIVE_TAPE_HEIGHT = 5.0; // 5 inches tall
